331a253d23a3faf9001c432e45a80d18c23dd15c
43trh <kayvan.farivar@gmail.com>
2024-10-27 05:28:35 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-var GHPATH = '/github-page-pwa';
-var APP_PREFIX = 'gppwa_';
-var VERSION = 'version_002';
+var GHPATH = '/nds-emu-github-page-pwa';
+var APP_PREFIX = 'ndsemupwa_';
+var VERSION = 'version_001';
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
